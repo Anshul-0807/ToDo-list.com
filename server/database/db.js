21b23ff49e5968e50b4c245bdf94c40c3626c57b
Anshul patle <patleanshul920@gmail.com>
2023-01-18 18:43:32 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,2 +1,23 @@
 import mongoose from 'mongoose';
 
+
+ const Connection = () => {
+
+  const MONGODB_URI = 'mongodb+srv://user:anshulpatletodo@mern-todo.nivx3vv.mongodb.net/?retryWrites=true&w=majority'
+    
+  mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+
+    // mongoose.connection.on('connected', () => {
+    //     console.log('Database connected successfully')
+    // });
+
+    // mongoose.connection.on('disconnected', () => {
+    //     console.log('Database disconnected')
+    // });
+
+    // mongoose.connection.on('error', () => {
+    //     console.log('Error while connecting the database', error.message);
+    // })
+}
+
+export default Connection;
