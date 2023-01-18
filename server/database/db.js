de79a44296c65b361e8975e1750c13e2a58f7fe5
Anshul patle <patleanshul920@gmail.com>
2023-01-19 00:24:19 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,23 +1,28 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
- const Connection = () => {
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
 
-  const MONGODB_URI = 'mongodb+srv://user:anshulpatletodo@mern-todo.nivx3vv.mongodb.net/?retryWrites=true&w=majority'
-    
-  mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+const Connection = () => {
 
-    // mongoose.connection.on('connected', () => {
-    //     console.log('Database connected successfully')
-    // });
+  const MONGODB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-kzl5gqm-shard-00-00.0q07k4v.mongodb.net:27017,ac-kzl5gqm-shard-00-01.0q07k4v.mongodb.net:27017,ac-kzl5gqm-shard-00-02.0q07k4v.mongodb.net:27017/?ssl=true&replicaSet=atlas-59p08i-shard-0&authSource=admin&retryWrites=true&w=majority`
 
-    // mongoose.connection.on('disconnected', () => {
-    //     console.log('Database disconnected')
-    // });
+  mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-    // mongoose.connection.on('error', () => {
-    //     console.log('Error while connecting the database', error.message);
-    // })
+  mongoose.connection.on('connected', () => {
+      console.log('Database connected Successfully');
+  })
+
+  mongoose.connection.on('disconnected', () => {
+      console.log('Database disconnected');
+  })
+
+  mongoose.connection.on('error', () => {
+      console.log('Error while connecting with the database ', error.message);
+  })
 }
 
 export default Connection;
