a4ea696ae0db52784dc47f33a7110f2a4703d2bb
Anshul patle <patleanshul920@gmail.com>
2023-01-18 18:43:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import Express from "express";
 
+import Connection from "./database/db.js";
+
 const app = Express();
 
 const port = 8000;
+
+Connection();
 
 app.listen(port, () => console.log(`your server is running succesfully on port ${port} `));
