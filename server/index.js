e1d35891e4dc49e8b42b2ed85365bafee52b0871
Anshul patle <patleanshul920@gmail.com>
2023-01-20 19:07:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 import Express from "express";
-
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import Connection from "./database/db.js";
+import Routes from "./routes/route.js";
 
 const app = Express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/', Routes);
 
 const port = 8000;
 
