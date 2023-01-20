dd3670bbe62c57939ea79aac1a2da29ce8732f44
Anshul patle <patleanshul920@gmail.com>
2023-01-20 19:07:13 +0530

    Update store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,17 @@
-import { createStore, combineReducers } from 'redux';
-
-
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { todosReducers } from './reducers/todosReducers';
 
 const reducer = combineReducers({
-    todos: todoReducers,
+    todos: todosReducers,
 })
 
-const store = createStore(
+const middleware = [thunk];
 
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
 ) 
+
+export default store;
