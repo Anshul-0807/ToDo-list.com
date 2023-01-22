00ef7b42f697eda6ab543f325d3c343bbfa1e42c
Anshul patle <patleanshul920@gmail.com>
2023-01-22 17:51:05 +0530

    Update todo-controller.js

--- server/controller/todo-controller.js
+++ server/controller/todo-controller.js
@@ -41,3 +41,27 @@ return response.status(200).json(todo);
 return response.status(500).json(error.message);
 }
 }
+
+export const updateTodo = async (request, response) => {
+    try{
+ await Todo.findOneAndUpdate(
+    {_id: request.params.id},
+    { data: request.body.data }
+ )
+ const todo = await Todo.findById(request.params.id);
+
+return response.status(200).json(todo);
+} catch(error){
+return response.status(500).json(error.message);
+}
+}
+
+export const deleteTodo = async (request, response) => {
+    try{
+ const todo = await Todo.findByIdAndDelete(request.params.id)
+
+return response.status(200).json(todo);
+} catch(error){
+return response.status(500).json(error.message);
+}
+}
