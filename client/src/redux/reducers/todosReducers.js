636f197a59853359315d7bcc387fa66a6472f68e
Anshul patle <patleanshul920@gmail.com>
2023-01-22 13:50:43 +0530

    Update todosReducers.js

--- client/src/redux/reducers/todosReducers.js
+++ client/src/redux/reducers/todosReducers.js
@@ -5,9 +5,12 @@ export const todosReducers = (state= [], action) => {
     switch (action.type) {
         case actionTypes.ADDNEW_TODO:
             return[action.payload, ...state]
-        case action.type.GETALL_TODO:
+        case actionTypes.GETALL_TODO:
             return action.payload
-
+        case actionTypes.TOGGLE_TODO:
+            return state.map(todo => (
+                todo._id === action.payload._id ? { ...todo, done: !todo.done } : todo
+            ))
         default:
         return state;
     }
