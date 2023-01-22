8578ecef05f9fdde1aaaa7cada9653a2f2430f85
Anshul patle <patleanshul920@gmail.com>
2023-01-22 13:50:32 +0530

    Update index.js

--- client/src/redux/action/index.js
+++ client/src/redux/action/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { ADDNEW_TODO, GETALL_TODO } from "./type";
+import { ADDNEW_TODO, GETALL_TODO, TOGGLE_TODO } from "./type";
 
 const API_URL = "http://localhost:8000";
 
@@ -22,4 +22,16 @@ export const getAllTodos = () => async (dispatch) => {
   } catch (error) {
     console.log("error while calling getAllTodos API", error.message);
   }
+  
+};
+
+export const toggleTodo = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`${API_URL}/todos/${id}`);
+
+    dispatch({ type: TOGGLE_TODO, payload: res.data });
+  } catch (error) {
+    console.log("error while calling getAllTodos API", error.message);
+  }
+  
 };
