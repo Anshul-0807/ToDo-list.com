9c54260a7ccbd4d587c21d102ae875fcccba8aa3
Anshul patle <patleanshul920@gmail.com>
2023-01-22 13:50:51 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { addTodo, getAllTodos } from "../controller/todo-controller.js";
+import { addTodo, getAllTodos, toggleTodoDone } from "../controller/todo-controller.js";
 const route = express.Router();
 
 route.post('/todos', addTodo)
 route.get('/todos', getAllTodos)
+route.get('/todos/:id', toggleTodoDone)
+
 
 export default route;
